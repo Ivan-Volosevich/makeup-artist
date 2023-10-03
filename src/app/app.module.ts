a5447149415a9d089bff38c8844fe1f7664f54e0
ivan volosevich <ivan.volosevich@vrpconsulting.com>
2023-10-03 16:00:06 +0300

    Added project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { TopComponent } from './components/home/top/top.component';
+import { ServicesComponent } from './components/home/services/services.component';
+import { ExamplesComponent } from './components/home/examples/examples.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    TopComponent,
+    ServicesComponent,
+    ExamplesComponent,
+    ContactsComponent,
   ],
   imports: [
     BrowserModule,
