e16aab509c11329d749b82c07bb3d12530774287
ivan volosevich <ivan.volosevich@vrpconsulting.com>
2023-10-06 18:17:13 +0300

    Started responsive design

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+  document: any;
+
+  viseOverflow(): void {
+    document.body.style.overflow = 'hidden';
+  }
+
+  hideOverflow(): void {
+    document.body.style.overflow = 'auto';
+  }
 }
